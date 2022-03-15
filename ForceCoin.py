2e2edf7f89c2a4ccef9ae30c012340d5ccd08973
AnDRoytEa <101189199+AnDRoytEa@users.noreply.github.com>
2022-03-16 00:13:55 +0300

    Add files via upload

---
+++ ForceCoin.py
@@ -0,0 +1,213 @@
+﻿import random
+
+from Script import Server
+from Script import Client
+from Script import Blockchain
+from Script import Wallet
+from Script import Block_that_needs_mining
+import sys
+import time
+import hashlib
+import os
+
+#cmd = 'mode 100,40'
+#os.system(cmd)
+
+
+
+
+
+
+
+
+
+hello_world = [
+'       .                                                                           ',
+'       |`.                                                                         ',
+'       ;  `.                                                                       ',
+'       ; :. \           __                                                         ',
+'       ; ; \ \      .--"  \                                                        ',
+'       ; ;  ; ;     :      \                                                       ',
+'       ; ;  : :     ; ;     ;                                                      ',
+'       \ :   ; ;    ;::     :                                                      ',
+'        \ \  : ;--.-;; l     ;                                                     ',
+'         \ \  ;:    :;// -.__:                                                     ',
+'         /\ \ ::____:::-\                                                          ',
+'        /  ).:+ """""""= \                                                         ',
+'       :_,=""     /"-.    ;                                                        ',
+'       ;"       .     `.  |                                                        ',
+'      :      .-^=    ==.\ |                                                        ',
+'      |  _.-".gp      gp:;:                                                        ',
+'      ;    /  $$      $$;: ;                                                       ',
+'     :    :  `--      --:  |                                                       ',
+'     ;    ;\        ,      |                                                       ',
+'    :    :  \      _   /   :   Семпай, войди в меня...                             ',
+'    ;    |   `.   `-  /     ;                                                      ',
+'   :     :    :`-.__.''     |                                                      ',
+'   ;          ;     :       |                                                      ',
+'  :     ...._/      ''__..  |                                                      ',
+'  ;   ./                  \ ;                                                      ',
+' :   /                   _ Y                                                       ',
+' ;  :         .g$$p.    d$$+.                                                      ',
+' ;  ;     :.g$$$$$$$$p.d$$$$$b                                                     ',
+' :  :     :$$$$$$$$$$$T$$$$$$$;                                                    ',
+'  \  ;    ;$$$$S$$$$$$$S$$$$$P                                                     ',
+'   `.|    |$$$$S$$$$$$$S$$$$P                                                      ',
+'     |    |T$$$$$SSSSS$$$$$$                                                       ',
+'     :    | `T$$$$$$$$$$$$$;                                                       ',
+'      ;   |   $$$$$$$$$$$$$                                                        ',
+'      |   :   $$$$$$$$$$$$;                                                        ',
+'      :    ; d$$$$$$$$$$$$;                                                        ',
+'      |    :d$$$$$$$$$$$$$$                                                        ',
+'      ;    :"^T$$$$$$$$$$$$"                                                       ',
+'     :     ;   `T$$$$$$$$$$P;                                                      ',
+'     ;    :      `T$$$$$$$P :                                                      ',
+'     |    ;        T$$$$$P   ;                                                     ',
+'     |   :          T$$P     ;                                                     ',
+'     :   ;           $       :                                                     ',
+'    ._;__;           :       :                                                     ',
+'    ; ;  ;           |       :                                                     ',
+'    :_L__:           |       ;                                                     ']
+for i in range(25):
+    print(hello_world[i])
+print('------------------------------')
+time.sleep(3)
+
+
+
+
+
+
+
+
+
+
+def login():
+    try:
+        f = open('Database/wallet.txt', 'r')                    # Чтение файла кошелька
+        wallet_name = f.readlines(1)
+        wallet_password = f.readlines(2)
+        wallet_key = f.readlines(3)
+        wallet_name = str(wallet_name[0][:-1])
+        wallet_password = str(wallet_password[0][:-1])
+        wallet_key = str(wallet_key[0][:-1])
+        f.close()
+        #print('Файл с данными кошелька существует')
+        Wallet.login(wallet_name, wallet_password, wallet_key)   # Вход через данные из файла кошелька
+    except:
+        #print('Файл с данными кошелька НЕ существует')
+        sys.stdout.write('Enter the wallet name: ')
+        wallet_name = input()
+        sys.stdout.write('Enter the wallet password: ')
+        wallet_password = input()
+
+        f = open('Database/wallet.txt', 'w')                      # Создание файла данных кошелька, запись данных
+        f.writelines(wallet_name + '\n')
+        f.writelines(wallet_password + '\n')
+        f.writelines(str(random.randint(0,1000000)) + '\n')
+        f.close()
+        print('Ваш кошелек успешно создан')
+
+        f = open('Database/wallet.txt', 'r')                      # Чтение данных из  кошелька
+        wallet_name = f.readlines(1)
+        wallet_password = f.readlines(2)
+        wallet_key = f.readlines(3)
+        wallet_name = str(wallet_name[0][:-1])
+        wallet_password = str(wallet_password[0][:-1])
+        wallet_key = str(wallet_key[0][:-1])
+        f.close()
+        Wallet.login(wallet_name, wallet_password, wallet_key)
+
+
+        Blockchain.download()                                      # Скачнивание блокчейна
+        Block_that_needs_mining.clear()                            # Очистка блоков которые нужно майнить
+        Block_that_needs_mining.download()                         # Скачнивание блоков которые нужно майнить
+        Block_that_needs_mining.create_bonus1000(Wallet.c)         # Если пользователь в первых 100 дается бонус
+
+        Wallet.login(wallet_name, wallet_password, wallet_key)     # Вход через данные из файла кошелька
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+Server.init()           # Проверка подключения и создание папок на серверах
+Client.init()           # Создание папок на устройстве клиента
+login()                 # Запуска входа в кошелек
+
+
+
+
+
+
+
+
+
+
+
+Blockchain.download()
+Block_that_needs_mining.clear()
+Block_that_needs_mining.download()
+
+
+#clear = lambda: os.system('cls')
+#clear()
+
+print('Адресс вашего кошелька')
+print(Wallet.c)
+Wallet.balance_info(Wallet.c)
+
+
+
+
+
+
+
+
+#pyinstaller.exe --onefile --icon=app.ico ForceCoin.py
+#pyinstaller -w -F -i "app.ico" ForceCoin.py
+
+
+
+#--------------------------------------------------------------------------
+#                 Отправка монет другому пльзователю
+sys.stdout.write('Кому хотите отправить монет?: ')
+user_recipient = input()
+sys.stdout.write('Сколько монет хотите отправить?: ')
+sent_coin = input()
+
+if (int(sent_coin) > int(Wallet.balance)):
+    print('Чурка, не пытайся меня обмануть')
+    quit()
+if  sent_coin<0:
+    print('Чурка, не пытайся меня обмануть')
+    quit()
+
+Blockchain.download()
+Block_that_needs_mining.clear()
+Block_that_needs_mining.download()
+Block_that_needs_mining.create(Wallet.c, user_recipient, sent_coin)
+#--------------------------------------------------------------------------
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ Script/Blockchain.py
@@ -0,0 +1,220 @@
+﻿import os
+import ftplib
+from Script import Server
+import  shutil
+import time
+import hashlib
+
+
+
+
+#-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+def download():                                                                # Скачивание всех блоков из блокчейна в свои папки
+    i = 0
+    z = 0
+    print('--------------------')
+    for i in range(Server.number_servers):                                           # Цикл от сервера №0 до последнего сервера
+        try:
+            ftp = ftplib.FTP(Server.host[i], Server.user[i], Server.password[i])     # Подключение к серверу
+            ftp.cwd('ForceCoin/Blockchain/')                                         # Переход в нужную деррикторию
+            #print('Успешно подключился к серверу', Server.host[i])
+        except:
+            pass
+            #print('Не удалось подключтся к серверу', host[i])
+        for z in range(1000000000):                                                  # Цикл для скачнивания блока от 0 до последнего
+            try:
+                file_name = str(z) + '.txt'                                          # Имя фалйа
+                my_file = open('Database/Blockchain_check/' + Server.host[i] + '/' + file_name, 'wb')  # Директория в котрую будет сохранен файл
+                ftp.retrbinary('RETR ' + file_name, my_file.write, 1024)             # Скачивание файла
+                print('SERVER', Server.host[i], 'блок', file_name, 'скачан')
+                my_file.close()                                                      # Закрытие директории в которую должен быть скачан файл
+            except:
+                my_file.close()                                                      # Закрытие директории в которую должен быть скачан файл
+                os.remove('Database/Blockchain_check/' + Server.host[i] + '/' + str(z) + '.txt')  # Удаление последнего бракованого блока
+                #print('SERVER', Server.host[i] ,'Все блоки скачаны')
+
+                break
+        print('--------------------')
+        ftp.close()                                                                  # Отключение от сервера
+# -------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+
+    check_1()
+    check_2()
+    check_3()
+    server_clean()
+    upload()
+
+
+
+
+#-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+def check_1():                                                     # Нахождение папки с максимальным количеством блоков
+    maxx = []                                                             # Максимальное количество блоков на сервере
+    for i in range(Server.number_servers):
+        blocks = []                                                       # Количество блоков на сервере
+        for z in range(1000000000):
+            try:
+                blocks.append(int(os.listdir('Database/Blockchain_check/' + Server.host[i] + '/')[z][:-4]))
+            except:
+                try:
+                    maxx.append(max(blocks))
+                except:
+                    maxx.append(0)
+                    maxx.append(-1)
+                break
+
+
+    #print(maxx)
+    #print(maxx.index(max(maxx)))
+    for i in range(1000000000):                                           # Цикл для скачнивания блока от 0 до последнего
+        try:
+            shutil.copyfile('Database/Blockchain_check/' + Server.host[maxx.index(max(maxx))] + '/' + str(i) + '.txt', 'Database/Blockchain_processing_1/' + str(i) + '.txt')
+            #print('Все супер')
+        except:
+            pass
+            #print('Копирование блоков в Blockchain_processing Завершено')
+            #print('--------------------')
+            break
+#-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+
+
+
+
+
+
+def check_2():
+    try:
+        for z in range(1000000000):
+            try:
+                file = open('Database/Blockchain_processing_1/' + str(z) + '.txt', 'r')
+                block = file.readlines()
+                file.close()
+                user_sender = block[0][13:-1]
+                sent_coin =  block[1][11:-1]
+                user_recipient = block[2][16:-1]
+                data = block[3][6:-1]
+                previous_hash = block[4][15:-1]
+                hash_key = block[5][10:-1]
+                this_hash = block[6][11:-1]
+                miner_name = block[7][12:]
+            except:
+                break
+
+            b = bytes(str(user_sender) + str(sent_coin) + str(user_recipient) + str(data) + str(previous_hash) + str(hash_key) + str(miner_name), encoding='utf-8')
+            hash = hashlib.sha256(b).hexdigest()
+            #print(this_hash)
+            #print(hash)
+
+            if hash == this_hash:
+                print('Хэш блока номер',z , 'праввильный')
+                shutil.copyfile('Database/Blockchain_processing_1/' + str(z) + '.txt', 'Database/Blockchain_processing_2/' + str(z) + '.txt')
+            else:
+                print('Хэш блока номер', z, 'не сходится, блок не правильный')
+    except:
+        print('неа')
+
+
+
+
+
+
+
+
+
+
+
+def check_3():                                            # Проверка правильная ли последовательность хэшей у блоков
+    for z in range(1000000000):                                # Проверка блоков на правильность
+        try:
+            file_z = open('Database/Blockchain_processing_1/' + str(z) + '.txt', 'r')             # открытие блока под номером i
+            file_z1 = open('Database/Blockchain_processing_1/' + str(z+1) + '.txt', 'r')          # открытие блока под номером i + 1
+
+            block_z = file_z.readlines()
+            block_z1 = file_z1.readlines()
+            file_z.close()
+            file_z1.close()
+            #print(block_z[5][11:])
+            #print(block_z1[4][15:])
+            #print(block_z[1][11:][:-1])
+
+
+            shutil.copyfile('Database/Blockchain_processing_2/' + str(z) + '.txt', 'Blockchain/' + str(z) + '.txt')
+            print('Блок номер', z, 'Добавлен в блокчейн')
+
+            if block_z[6][11:] == block_z1[4][15:]:  # Цикл для проверки предыдущих хэшей блока
+                shutil.copyfile('Database/Blockchain_processing_2/' + str(z+1) + '.txt', 'Blockchain/' + str(z+1) + '.txt')
+                print('Блок номер', z, 'Добавлен в блокчейн')
+            else:
+
+                break
+        except:
+            break
+                # print('неа')
+        file_z.close()
+        file_z1.close()
+
+
+
+#-------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------------
+
+
+
+
+def client_clean():
+    for i in range(Server.number_servers):
+        for z in range(1000000000):
+            try:
+                os.remove('Database/Blockchain_check/' + Server.host[i] + '/' + str(z) + '.txt')
+                print('Из папки Database/Blockchain_check/', Server.host[i], 'блок', str(z) + '.txt', 'удален')
+            except:
+                break
+    for z in range(1000000000):
+        try:
+            print(z)
+            os.remove('Database/Blockchain_processing/' + str(z) + '.txt')
+            print('Из папки Database/Blockchain_processing/', Server.host[i], 'блок', str(z) + '.txt', 'удален')
+        except:
+            break
+
+
+
+def server_clean():
+    for i in range(Server.number_servers):
+        ftp = ftplib.FTP(Server.host[i], Server.user[i], Server.password[i])  # Конект к серверу
+        ftp.cwd('ForceCoin/Blockchain/')
+        for z in range(1000000000):
+            try:
+                ftp.delete(str(z) + '.txt')
+                print('SERVER', Server.host[i], 'блок', str(z) + '.txt', 'удален')
+            except:
+                break
+
+
+
+def upload():
+    for i in range(Server.number_servers):
+        ftp = ftplib.FTP(Server.host[i], Server.user[i], Server.password[i])
+        ftp.cwd('/ForceCoin/Blockchain/')
+        for z in range(1000000000):
+            try:
+                file_name = str(z) + '.txt'                                                   # Имя фала
+                my_file = open('Blockchain/' + file_name, 'rb')                               # Директория фала
+                ftp.storbinary('STOR ' + file_name, my_file)                                  # Загрузка фала на сервер
+                #print('Блок для майнинга', file_name, 'загружен на сервер', Server.host[i])
+                my_file.close()
+            except:
+                #print('SERVER', Server.host[i], 'не работает')
+                break
+        ftp.close()
+
+
+
+
+
+
+
+
+
+
