2f78ba2181682c5d407718d9042ccc8cffea0173
AnDRoytEa <101189199+AnDRoytEa@users.noreply.github.com>
2022-05-29 13:08:17 +0300

    Add files via upload

---
+++ LEGION/LEGION_cmd.py
@@ -0,0 +1,86 @@
+﻿import os
+from System.Source import ForceNET as FNET
+import time
+
+
+
+
+
+a = FNET.server_init()
+print('ForceNET - server_init() ----',a)
+
+if a == 'failed':
+    print('You need update the ForceNET system')
+    time.sleep(1000)
+
+z = FNET.client_folder_init()
+print('ForceNET - folder_init(client) ----',z)
+
+z = FNET.create_wallet()
+print('ForceNET - wallet(create) ----',z)
+
+z = FNET.server_folder_init()
+print('ForceNET - folder_init(server) ----',z)
+
+z = FNET.wallet_init()
+print('ForceNET - wallet(init) ----',z)
+
+z = FNET.download('blockchain')
+print('ForceNET - download(blockchain) ----',z)
+
+FNET.processing('blockchain_processing_0')
+FNET.processing('blockchain_processing_1')
+FNET.processing('blockchain_processing_2')
+FNET.processing('blockchain_processing_3')
+
+z = FNET.download('block_that_needs_mining')
+print('ForceNET - download(block_that_needs_mining) ----',z)
+
+z = FNET.upload('blockchain')
+print('ForceNET - upload(blockchain) ----',z)
+
+z = FNET.upload('block_that_needs_mining')
+print('ForceNET - upload(block_that_needs_mining) ----',z)
+
+
+
+
+
+print('')
+while True:
+    a = input('ForceNET>> ')
+    print(a)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
